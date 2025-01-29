8be73f30ef606c0ae71b938bc5669581c5d89e26
Seb <121176289+sebDtSci@users.noreply.github.com>
2025-01-29 13:54:11 +0100

    rule update

---
+++ src/main/java/com/example/Res.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Res {
+    private boolean major;
+
+    public Res(){}
+
+    public Res(boolean major) {
+        this.major = major;
+    }
+
+    public boolean getMajor() {
+        return major;
+    }
+
+    public void setMajor(boolean major) {
+        this.major = major;
+    }
+    
+}
--- src/main/java/com/example/RuleUnit.java
+++ src/main/java/com/example/RuleUnit.java
@@ -7,16 +7,26 @@ import org.drools.ruleunits.api.SingletonStore;
 public class RuleUnit implements RuleUnitData {
 
     private SingletonStore<Person> personne;
+    private SingletonStore<Res> res;
 
     public RuleUnit() {
         this.personne = DataSource.createSingleton();
+        this.res = DataSource.createSingleton();
     }
 
     public SingletonStore<Person> getPersonne() {
         return personne;
     }
+    
+    public SingletonStore<Res> getRes() {
+        return res;
+    }
 
     public void setPersonne(SingletonStore<Person> personne) {
         this.personne = personne;
+    }
+    
+    public void setRes(SingletonStore<Res> res) {
+        this.res = res;
     }
 }
